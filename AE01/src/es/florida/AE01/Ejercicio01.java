0d07b8179766ca0c703c29870d61ccbfe9e60174
J_M_Dominguez <jmdominguezhernandez@gmail.com>
2021-10-15 23:36:16 +0200

    Ejercicio01 completo

---
+++ AE01/src/es/florida/AE01/Ejercicio01.java
@@ -0,0 +1,23 @@
+package es.florida.AE01;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1. Crea una aplicación de consola en la se utilice una clase que tenga un método “sayHello” \r\n"
+				+ "   que saque por consola “Hola Mundo”. La clase puede tener el nombre que consideres. Como \r\n"
+				+ "   sugerencia, la clase se puede llamar “App”.\r\n");
+		
+		System.out.println(sayHello());
+	
+	}
+/*	Comment
+	INPUT: 	se invoca sin parámetro
+	OUTPUT: String con la frase       */
+	public static String sayHello() {
+		return "Hola Mundo";
+	}
+}
+	
+
+
