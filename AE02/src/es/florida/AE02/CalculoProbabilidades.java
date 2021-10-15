23521aba813c8ade0b8d9da8e4a98d95d828919a
J_M_Dominguez <jmdominguezhernandez@gmail.com>
2021-10-15 23:57:45 +0200

    Clase CalculoProbabilidades terminada

--- AE02/src/es/florida/AE02/CalculoProbabilidades.java
+++ AE02/src/es/florida/AE02/CalculoProbabilidades.java
@@ -3,8 +3,28 @@ package es.florida.AE02;
 public class CalculoProbabilidades {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double posNEO = 1.1;
+		double velNEO = 10.1;
+		double probabilidadImpacto = calculoProbabilidad(posNEO, velNEO);
 
+		System.out.println(probabilidadImpacto);
+	}
+	
+	
+	
+	public static double calculoProbabilidad (double posicionNEO, double velocidadNEO) {
+		
+		double posicionTierra = 1;
+		double velocidadTierra = 100;
+		for (int i = 0; i< (50*365*24*60*60); i++) {
+			posicionNEO = posicionNEO + velocidadNEO * i;
+			posicionTierra = posicionTierra + velocidadTierra * i;
+		}
+		double probabilidad = 100 * Math.random() * Math.pow((posicionNEO - posicionTierra)/(posicionNEO + posicionTierra), 2);
+		double probabilidadFormat = Math.round(probabilidad * 100.0) / 100.0;
+		
+		return probabilidadFormat;
 	}
 
 }
